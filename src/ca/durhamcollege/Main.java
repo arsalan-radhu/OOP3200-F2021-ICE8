dd578fdccc1b61ab18881653d0a6a81013f04805
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-11-10 12:47:42 -0500

    Initial commit and Final

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,81 @@
+//
+// Name: Arsalan Arif Radhu
+// Student ID: 100813965
+// Description: ICE 8
+
+package ca.durhamcollege;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        // define our Scanner
+        Scanner keyboard = new Scanner(System.in);
+
+        // Input
+        String name = null;
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+
+        float age = 0.0f;
+        boolean isValidInput = false; // sentinel variable
+
+        do
+        {
+            System.out.print("Please enter your age: ");
+            try
+            {
+
+                age = keyboard.nextFloat();
+                isValidInput = true;
+
+                if((age <= 0.0f) || (age >= 120.0f))
+                {
+                    System.out.println("Error: You must enter an age greater than 0.0 and less than 120.0 years old");
+                    keyboard.nextLine();
+                    isValidInput = false;
+                }
+            }
+            catch(InputMismatchException inputMismatchException)
+            {
+                System.out.println("Error: You must enter a valid floating-point number");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+
+        }
+        while(!isValidInput);
+
+
+        // multi-dimensional array example
+
+        System.out.println("Multi-Dimensional Array example");
+        int rows = 8;
+        int cols = 8;
+        float[][] board = new float[rows][cols];
+
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < cols; y++) {
+                board[x][y] = (float)(Math.random() * 100.0) + 1.0f;
+            }
+        }
+
+        System.out.println("Showing 10 Random Values: ");
+
+        for (int i = 0; i < 10; i++) {
+            var randRows = (int)(Math.random() * rows);
+            var randCols = (int)(Math.random() * cols);
+            System.out.print("For row: " + randRows + " col: " + randCols + " ");
+            System.out.printf("The Random number is: %.2f \n", board[randRows][randCols]);
+        }
+
+        // Output
+        System.out.println();
+        System.out.printf("You Entered: %s \n",name);
+        System.out.printf("You Entered: %.2f \n",age);
+    }
+}
